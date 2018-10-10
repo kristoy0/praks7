03b0d24b3288840861f56dfeb54b92f547c7654b
kristoy0 <kristoy0@hotmail.com>
2018-10-10 13:43:23 +0300

    Lisatud sisestatavate numbrite kohtade arvu limiit ja "nightmode".

--- main.js
+++ main.js
@@ -4,16 +4,31 @@ let gramOutput = document.getElementById('gramOutput');
 let poundOutput = document.getElementById('poundOutput');
 let ozOutput = document.getElementById('ozOutput');
 let output = document.getElementById('output');
+let nightToggle = document.getElementById('nightToggle');
+let body = document.querySelector('body');
 
+let nightOn = false;
 output.style.visibility = 'hidden';
 
 // Listening to events
 kgInput.addEventListener('input', event => {
     let kg = event.target.value;
-
+    
     output.style.visibility = (kg === '0') ? 'hidden' : 'visible';
     
     gramOutput.innerHTML = `${kg * 1000} g`;
     poundOutput.innerHTML = `${(kg * 2.20462262).toFixed(2)} lb`
     ozOutput.innerHTML = `${(kg * 35.2739619).toFixed(2)} oz`
 });
+
+nightToggle.addEventListener('click', event => {
+    nightOn = !nightOn;
+    
+    if (nightOn) {
+        body.style.backgroundColor = 'black';
+        body.style.color = 'white';
+    } else {
+        body.style.backgroundColor = 'white';
+        body.style.color = 'black';
+    }
+})
